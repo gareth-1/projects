42b93329f68392448e2364a814d91a3cc4a51780
gareth-1 <116625599+gareth-1@users.noreply.github.com>
2022-10-26 20:58:58 +0800

    Add files via upload

---
+++ age teller.py
@@ -0,0 +1,6 @@
+year_of_birth = int(input("what is your year of birth in yyyy format?"))
+current_year = int(input("what is the current year?"))
+age = current_year - year_of_birth 
+print(age , "years old")
+
+
