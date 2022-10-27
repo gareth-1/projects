18187cc850762b0691fea1a694c329c061a37e26
gareth-1 <116625599+gareth-1@users.noreply.github.com>
2022-10-27 12:32:16 +0800

    Delete rectangle area finder.py

--- rectangle area finder.py
+++
@@ -1,5 +0,0 @@
-length = int(input("length of rectangle in cm:"))
-width = int(input("width of rectangle in cm:")) 
-area = length*width 
-perimeter = length*2 + width*2
-print(area , "cm^2" , "and perimeter", perimeter)
