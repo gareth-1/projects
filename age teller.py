e52285267e6e8f252e20188e495842b1a95eed46
gareth-1 <116625599+gareth-1@users.noreply.github.com>
2022-10-27 12:34:11 +0800

    Delete age teller.py

--- age teller.py
+++
@@ -1,6 +0,0 @@
-year_of_birth = int(input("what is your year of birth in yyyy format?"))
-current_year = int(input("what is the current year?"))
-age = current_year - year_of_birth 
-print(age , "years old")
-
-
