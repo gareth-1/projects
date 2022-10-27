1eb3017a845fe1eeceacaeb5f07f669af2cafec7
gareth-1 <116625599+gareth-1@users.noreply.github.com>
2022-10-27 12:02:51 +0800

    Add files via upload

--- rectangle area finder.py
+++ rectangle area finder.py
@@ -1,4 +1,5 @@
 length = int(input("length of rectangle in cm:"))
 width = int(input("width of rectangle in cm:")) 
 area = length*width 
-print(area , "cm^2")
+perimeter = length*2 + width*2
+print(area , "cm^2" , "and perimeter", perimeter)
