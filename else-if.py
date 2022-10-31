a4c3f6dc323eda002f2d57dc719b60ce7c1da403
gareth-1 <116625599+gareth-1@users.noreply.github.com>
2022-10-31 10:41:20 +0800

    Update else-if.py
    added comments for better reading

--- else-if.py
+++ else-if.py
@@ -2,10 +2,15 @@
 #initialisation 
 age = 0 
 
+#variable 1 
 age = int(input("please enter your age : "))
+
+#if-else code 
 if age >= 60 :
     print("Congrats senior citizen, you get a bonus of $500")
 elif age < 60 :
     print("Congrats citizen, you get a bonus of $200")
-print("everyone gets a free health check")
+
+    #to be carried out regardless of if-else code 
+    print("everyone gets a free health check")
 
